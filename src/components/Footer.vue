56347843b4cd0e826da3978dd11a7ce5b212887f
Anas Sibarni <anas.sibarni.etu@univ-lille.fr>
2025-12-08 10:01:24 +0100

    correction d'une faute dans le footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
   <footer class="site-footer">
     <div class="footer-content">
       <p class="availability">
-        En echerche active d'un stage et d'une alternance en développement d'applications.
+        En recherche active d'un stage et d'une alternance en développement d'applications.
       </p>
 
       <p class="contact-info">
